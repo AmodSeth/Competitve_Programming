3a0a4becf02c172f7ce2ccf89d5a0731d5be6f61
AmodSeth <amodseth2002@gmail.com>
2025-05-07 20:50:31 +0530

    C++ for mac setup

---
+++ C++/demo.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define pb push_back
+#define fin(i,n) for(int i=0;i<n;i++)
+using namespace std;
+void solve()
+{
+	int x,y; cin>>x>>y;
+    cout<<x<<" "<<y<<endl;
+
+
+
+
+
+
+
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
